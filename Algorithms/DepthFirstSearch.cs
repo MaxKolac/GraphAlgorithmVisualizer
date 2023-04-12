94f782fad31c44ed005955ff7f4c94dbc4f573f3
MaxKolac <maksymilian.kolacinski@gmail.com>
2023-04-12 20:24:19 +0200

    Algorithm should work OK now

--- Algorithms/DepthFirstSearch.cs
+++ Algorithms/DepthFirstSearch.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using GraphAlgorithmVisualizer.Exceptions;
 using GraphAlgorithmVisualizer.MathObjects;
@@ -8,17 +9,19 @@ namespace GraphAlgorithmVisualizer.Algorithms
     internal class DepthFirstSearch
     {
         private readonly Dictionary<Vertex, bool> visited;
-        private readonly Dictionary<Vertex, int> outgoingUnvisitedEdges;
+        private readonly Dictionary<Vertex, Vertex> previousVertex;
+        private readonly Dictionary<Vertex, List<Vertex>> outgoingEdges;
         private readonly Graph graph;
 
         public bool WasVisited(Vertex v) => visited[v];
-        public bool IsCrossroad(Vertex v) => outgoingUnvisitedEdges[v];
+        public Vertex PreviousVertexOf(Vertex v) => previousVertex[v];
         public Vertex StartVertex { get; private set; }
 
         public DepthFirstSearch(Graph graph) 
         { 
             visited = new Dictionary<Vertex, bool>();
-            outgoingUnvisitedEdges = new Dictionary<Vertex, int>();
+            previousVertex = new Dictionary<Vertex, Vertex>();
+            outgoingEdges = new Dictionary<Vertex, List<Vertex>>();
             this.graph = graph;
         }
 
@@ -28,67 +31,52 @@ namespace GraphAlgorithmVisualizer.Algorithms
                 throw new GraphException("Start Vertex was not found on Graph's Vertices list.");
 
             visited.Clear();
-            outgoingUnvisitedEdges.Clear();
+            previousVertex.Clear();
+            outgoingEdges.Clear();
             StartVertex = start;
             foreach (Vertex v in graph.VerticesArray)
             {
                 visited.Add(v, false);
-                outgoingUnvisitedEdges.Add(v, 0);
+                previousVertex.Add(v, null);
+                outgoingEdges.Add(v, new List<Vertex>());
             }
 
             GoDownBranch(start);
-            bool continueLoop = true;
-            while (continueLoop)
+            while (visited.Values.Contains(false))
             {
-                continueLoop = false;
-                foreach (Vertex vertex in graph.VerticesArray)
+                foreach (KeyValuePair<Vertex, bool> kvp in visited)
                 {
-                    ///////??????/
-                    if (!visited[vertex])
-                    {
-                        continueLoop = true;
-                        GoDownBranch(vertex);
-                    }
+                    if (kvp.Value == false)
+                        GoDownBranch(kvp.Key);
                 }
             }
         }
 
         /// <summary>
         /// Performs one iteration of "going down" a branch of the <c>Graph</c> from the starting Vertex.
-        /// As it goes down the vertices, it marks them as "visited" and whether or not they are considered "crossroads" a.k.a. have more than 1 Edge coming out of them and will need to be revisited again.
+        /// As it goes down the vertices, it marks them as "visited" and tracks which vertices should be visited next from the current vertex. Once a list of those vertices is fulfilled, method enters a recursion and starts "going down" a branch from each connected Vertex.
         /// Once it reaches a Vertex with no Edges other than the one the algorithm went through to reach this Vertex, it returns.
         /// </summary>
         /// <param name="v">The starting Vertex from which the method starts from.</param>
-        private void GoDownBranch(Vertex v)
+        private void GoDownBranch(Vertex currentVertex)
         {
-            Vertex currentVertex = v;
-            Vertex matchedEdgesVertex = null;
-            Vertex otherEdgesVertex = null;
-            //List<Edge> connectingUnvisitedEdges = new List<Edge>();
-
-            do
+            visited[currentVertex] = true;
+            foreach (Edge edge in graph.EdgesArray)
             {
-                visited[currentVertex] = true;
-                //connectingUnvisitedEdges.Clear();
-
-                foreach (Edge edge in graph.EdgesArray)
+                if (!edge.IsStartingFrom(currentVertex))
+                    continue;
+                Vertex matchedEdgesVertex = edge.Start.Equals(currentVertex) ? edge.Start : edge.End;
+                Vertex otherEdgesVertex = edge.Start.Equals(currentVertex) ? edge.End : edge.Start;
+                if (!visited[otherEdgesVertex])
                 {
-                    if (!edge.IsStartingFrom(currentVertex))
-                        continue;
-                    matchedEdgesVertex = edge.Start.Equals(currentVertex) ? edge.Start : edge.End;
-                    otherEdgesVertex = edge.Start.Equals(currentVertex) ? edge.End : edge.Start;
-                    if (!visited[otherEdgesVertex])
-                        //connectingUnvisitedEdges.Add(edge);
-                        outgoingUnvisitedEdges[matchedEdgesVertex]++;
+                    previousVertex[otherEdgesVertex] = currentVertex;
+                    outgoingEdges[matchedEdgesVertex].Add(otherEdgesVertex);
                 }
-                //if (connectingUnvisitedEdges.Count >= 1)
-                if (outgoingUnvisitedEdges[matchedEdgesVertex] > 0)
-                    currentVertex = otherEdgesVertex;
-                //if (connectingUnvisitedEdges.Count > 1)
-                //outgoingUnvisitedEdges[currentVertex] = true;
-                //} while (connectingUnvisitedEdges.Count > 0);
-            } while (outgoingUnvisitedEdges[currentVertex] > 0);
+            }
+            foreach (Vertex nextVertex in outgoingEdges[currentVertex])
+                GoDownBranch(nextVertex);
         }
+
         /// <returns>
         /// A formatted <c>String</c> of the contents of <c>visited</c> and <c>isCrossroad</c> dictionaries.
         /// </returns>
@@ -99,9 +87,18 @@ namespace GraphAlgorithmVisualizer.Algorithms
             foreach (KeyValuePair<Vertex, bool> pair in visited)
                 builder.AppendLine($"\t{pair.Key}\t|\t{pair.Value}");
 
-            builder.AppendLine("isCrossroad Dictionary: ");
-            foreach (KeyValuePair<Vertex, bool> pair in outgoingUnvisitedEdges)
+            builder.AppendLine("previousVertex Dictionary: ");
+            foreach (KeyValuePair<Vertex, Vertex> pair in previousVertex)
                 builder.AppendLine($"\t{pair.Key}\t|\t{pair.Value}");
+
+            builder.AppendLine("outgoingEdges Dictionary: ");
+            foreach (KeyValuePair<Vertex, List<Vertex>> pair in outgoingEdges)
+            {
+                builder.Append($"\t{pair.Key}\t|\t");
+                foreach (Vertex v in pair.Value)
+                    builder.Append($"{v} ");
+                builder.AppendLine();
+            }
             return builder.ToString();
         }
     }
