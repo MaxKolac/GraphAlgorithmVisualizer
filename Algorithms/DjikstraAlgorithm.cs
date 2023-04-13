b759f93d77d1ceab200db44fa8e97dcc080af0bf
MaxKolac <maksymilian.kolacinski@gmail.com>
2023-04-13 20:55:56 +0200

    Djikstra's algorithm should be OK now. Introduced custom version of Queue for Djikstra's algorithm, aka DjikstraQueue.

--- Algorithms/DjikstraAlgorithm.cs
+++ Algorithms/DjikstraAlgorithm.cs
@@ -1,51 +1,82 @@
-﻿using GraphAlgorithmVisualizer.MathObjects;
-using System.Text;
+﻿using System.Collections.Generic;
+using GraphAlgorithmVisualizer.MathObjects;
 
 namespace GraphAlgorithmVisualizer.Algorithms
 {
     internal class DjikstraAlgorithm : Algorithm
     {
-        private readonly PriorityHeap<Vertex> heap;
-
         public DjikstraAlgorithm(Graph graph) : base(graph)
         {
-            heap = new PriorityHeap<Vertex>();
         }
 
         public override void Perform(Vertex start)
         {
             SetStartVertex(start);
-            ClearDictionaries();
+            ClearDictionaries(true);
+            distance[start] = 0;
 
-            heap.Clear();
+            DjikstraQueue<Vertex> djikstraQueue = new DjikstraQueue<Vertex>();
             foreach (Vertex v in graph.VerticesArray)
-                heap.Insert(v);
+                djikstraQueue.Enqueue(v);
 
             do
             {
-                Vertex currentVertex = heap.DeleteMin();
+                Vertex currentVertex = djikstraQueue.Dequeue();
                 visited[currentVertex] = true;
                 foreach (Edge edge in graph.EdgesArray)
                 {
                     if (!edge.IsStartingFrom(currentVertex))
                         continue;
-                    if (distance[edge.End] > distance[edge.Start] + edge.Distance)
+                    Vertex matchedEdgesVertex = edge.Start.Equals(currentVertex) ? edge.Start : edge.End;
+                    Vertex otherEdgesVertex = edge.Start.Equals(currentVertex) ? edge.End : edge.Start;
+                    if (distance[otherEdgesVertex] > distance[matchedEdgesVertex] + edge.Distance)
                     {
-                        distance[edge.End] = distance[edge.Start] + (edge.Distance ?? 0);
-                        previousVertex[edge.End] = edge.Start;
-                        heap.DecreaseKey(edge.End);
+                        distance[otherEdgesVertex] = distance[matchedEdgesVertex] + (edge.Distance ?? 0);
+                        previousVertex[otherEdgesVertex] = matchedEdgesVertex;
+                        djikstraQueue.PushDown(otherEdgesVertex);
                     }
                 }
-            } while (!heap.IsEmpty);
+            } while (!djikstraQueue.IsEmpty);
+        }
+    }
+
+    internal class DjikstraQueue<T>
+    {
+        private readonly Queue<T> queue;
+        public int Count => queue.Count;
+        public bool IsEmpty => queue.Count == 0;
+
+        public DjikstraQueue()
+        {
+            queue = new Queue<T>();
         }
 
-        public override string ToString()
+        public void Enqueue(T item) => queue.Enqueue(item);
+        public T Dequeue() => queue.Dequeue();
+        /// <summary>
+        /// Swaps the selected item down, closer to the start of the queue by one position.
+        /// </summary>
+        /// <param name="item">The element that will be pushed down the queue.</param>
+        public void PushDown(T item)
         {
-            StringBuilder builder = new StringBuilder();
-            builder.AppendLine(base.ToString());
-            builder.AppendLine("heap contents: ");
-            builder.AppendLine(heap.ToString());
-            return builder.ToString();
+            if (!queue.Contains(item)) return;
+            if (queue.Peek().Equals(item)) return;
+
+            Queue<T> spareQueue = new Queue<T>();
+            while (!queue.Peek().Equals(item))
+                spareQueue.Enqueue(queue.Dequeue());
+
+            int queueCount = queue.Count;
+            int spareQueueCount = spareQueue.Count;
+
+            for (int i = 0; i < spareQueueCount - 1; i++)
+                queue.Enqueue(spareQueue.Dequeue());
+
+            queue.Enqueue(queue.Dequeue());
+            queue.Enqueue(spareQueue.Dequeue());
+
+            for (int i = 0; i < queueCount - 1; i++)
+                queue.Enqueue(queue.Dequeue());
         }
     }
 }
