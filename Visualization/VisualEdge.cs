98f339aff3602cc178d2e1a1c88e3422f7b93883
MaxKolac <maksymilian.kolacinski@gmail.com>
2023-04-14 22:07:23 +0200

    Added visual versions of MathObjects

---
+++ Visualization/VisualEdge.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+
+namespace GraphAlgorithmVisualizer.Visualization
+{
+    internal class VisualEdge : VisualObject
+    {
+        public readonly VisualVertex Start;
+        public readonly VisualVertex End;
+        public readonly bool IsDirectional;
+        public readonly int? Distance;
+
+        public VisualEdge(VisualVertex start, VisualVertex end, bool isDirectional, int? distance) : base((start.X + end.X) / 2, (start.Y + end.Y) / 2)
+        {
+            Start = start;
+            End = end;
+            IsDirectional = isDirectional;
+            Distance = distance;
+        }
+
+        public override void Draw(Graphics canvas)
+        {
+            canvas.DrawLine(pen, Start.X, Start.Y, End.X, End.Y);
+            if (IsDirectional)
+            {
+                //TODO: draw arrow's end
+            }
+            if (Distance is null) return;
+            //TODO: draw distance String
+        }
+    }
+}
---
+++ Visualization/VisualObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Drawing;
+
+namespace GraphAlgorithmVisualizer.Visualization
+{
+    internal abstract class VisualObject
+    {
+        public int X;
+        public int Y;
+
+        protected Pen pen = new Pen(Color.Black, 3);
+        protected Font font = new Font(FontFamily.GenericSansSerif, 8);
+
+        protected VisualObject(int x, int y)
+        {
+            MoveTo(x, y);
+        }
+        
+        protected VisualObject(Point p)
+        {
+            MoveTo(p.X, p.Y);
+        }
+
+        /// <summary>
+        /// Moves the object to the specified (X,Y) coordinates.
+        /// </summary>
+        public void MoveTo(int x, int y)
+        {
+            X = x; 
+            Y = y;
+        }
+        /// <summary>
+        /// Draws the visualizable object on the specified canvas on the (X,Y) coordinates.
+        /// </summary>
+        public abstract void Draw(Graphics canvas);
+    }
+}
