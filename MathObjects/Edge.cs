4d748ea40a0ab3322ef41560b8ad0da0ac25fe8c
MaxKolac <maksymilian.kolacinski@gmail.com>
2023-04-16 15:58:19 +0200

    Added initial Draw() implementation Cleared up constructors regarding the Distance parameter.

--- MathObjects/Edge.cs
+++ MathObjects/Edge.cs
@@ -1,14 +1,16 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
+using System.Text;
 using GraphAlgorithmVisualizer.Exceptions;
 using GraphAlgorithmVisualizer.Visualization;
 
 namespace GraphAlgorithmVisualizer.MathObjects
 {
+    /// <summary>
+    /// A connection between two vertices.
+    /// </summary>
     internal class Edge : IVisualizable
     {
-        private Point position;
-        public int X { get { return position.X; } set { position = new Point(position.X, value); } }
-        public int Y { get { return position.Y; } set { position = new Point(value, position.Y); } }
 
         /// <summary>
         /// <c>Vertex</c> from which the <c>Edge</c> begins. Only matters if the <c>Edge</c> is directional.
@@ -27,11 +29,14 @@ namespace GraphAlgorithmVisualizer.MathObjects
         /// </summary>
         public readonly int? Distance;
 
-        public Edge(Vertex start, Vertex end, bool isDirectional, int? distance)
+        public Edge(Vertex start, Vertex end, bool isDirectional)
         {
             Start = start;
             End = end;
             IsDirectional = isDirectional;
+        }
+        public Edge(Vertex start, Vertex end, bool isDirectional, int distance) : this(start, end, isDirectional)
+        {
             if (distance <= 0)
                 throw new GraphException("Attempted to create an Edge with non-positive Distance.");
             Distance = distance;
@@ -56,16 +61,51 @@ namespace GraphAlgorithmVisualizer.MathObjects
         /// </returns>
         public bool IsEndingIn(Vertex v) => IsDirectional ? End.Equals(v) : Start.Equals(v) || End.Equals(v);
 
-        public void MoveTo(int x, int y) => position = new Point(x, y);
         public void Draw(Graphics graphics)
         {
-            graphics.DrawLine(DrawingTools.GlobalPen, Start.X, Start.Y, End.X, End.Y);
-            if (IsDirectional)
-            {
-                //TODO: draw arrow's end
-            }
-            if (Distance is null) return;
-            //TODO: draw distance String
+            graphics.DrawLine(DrawingTools.DefaultOutline, Start.X, Start.Y, End.X, End.Y);
+            graphics.DrawString($"{Distance}", DrawingTools.DefaultFont, DrawingTools.DefaultFontColor, new Point(Math.Abs(End.X + Start.X) / 2, Math.Abs(End.Y + Start.Y) / 2));
+
+            if (!IsDirectional) return;
+            //This is a mathematical disgrace and a mess and if my university teachers saw this they would throw me out INSTANTLY, but god DAMNIT it works...
+
+            double ArrowArmAngle = Extensions.ToRadians(15d);
+            int ArrowArmLength = 15;
+            int LineEndOffset = 10; //This needs to be Vertex's size / 2
+
+            //TODO: Despite this mess, QuarterAngleOffset still doesn't quite work
+            //Mafs is hard :p, not too proud out of this "wooden else-if'ing"...
+            double QuarterAngleOffset;
+            if (End.X > Start.X && End.Y <= Start.Y)
+                QuarterAngleOffset = Extensions.ToRadians(0);
+            else if (End.X <= Start.X && End.Y < Start.Y)
+                QuarterAngleOffset = Extensions.ToRadians(90);
+            else if (End.X < Start.X && End.Y >= Start.Y)
+                QuarterAngleOffset = Extensions.ToRadians(180);
+            else //if (End.X >= Start.X && End.X > Start.Y)
+                QuarterAngleOffset = Extensions.ToRadians(270);
+
+            int DifferenceY = End.Y - Start.Y;
+            int DifferenceX = End.X - Start.X;
+            double Alpha = (DifferenceX == 0 || DifferenceY == 0) ? QuarterAngleOffset : Math.Atan(Math.Abs(DifferenceY) / Math.Abs(DifferenceX)) + QuarterAngleOffset;
+            double Beta = Extensions.ToRadians(90d - Extensions.ToDegrees(Alpha));
+            Point ArrowArmStartPoint = new Point(End.X - (int)Math.Round(LineEndOffset * Math.Sin(Beta)), End.Y + (int)Math.Round(LineEndOffset * Math.Sin(Alpha)));
+
+            int ArrowOneX = ArrowArmStartPoint.X - (int)Math.Round(ArrowArmLength * Math.Sin(Beta - ArrowArmAngle));
+            int ArrowOneY = ArrowArmStartPoint.Y + (int)Math.Round(ArrowArmLength * Math.Cos(Beta - ArrowArmAngle));
+            int ArrowTwoX = ArrowArmStartPoint.X - (int)Math.Round(ArrowArmLength * Math.Sin(Beta + ArrowArmAngle));
+            int ArrowTwoY = ArrowArmStartPoint.Y + (int)Math.Round(ArrowArmLength * Math.Cos(Beta + ArrowArmAngle));
+
+            graphics.DrawLine(DrawingTools.DefaultOutline, ArrowArmStartPoint, new Point(ArrowOneX, ArrowOneY));
+            graphics.DrawLine(DrawingTools.DefaultOutline, ArrowArmStartPoint, new Point(ArrowTwoX, ArrowTwoY));
+
+            //Debug
+            /*StringBuilder builder = new StringBuilder();
+            builder.AppendLine(this.ToString());
+            builder.AppendLine($"Alpha: {Alpha} ({Extensions.ToDegrees(Alpha)})");
+            builder.AppendLine($"Beta: {Beta} ({Extensions.ToDegrees(Beta)})");
+            builder.AppendLine($"ArrowArmStartPoint: {ArrowArmStartPoint}");
+            Console.WriteLine(builder.ToString());*/
         }
 
         public override string ToString()
