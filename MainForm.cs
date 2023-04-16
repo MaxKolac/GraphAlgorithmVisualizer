7cc83f763d2cf3e22ba0fe1bdde20f6738e3dd76
MaxKolac <maksymilian.kolacinski@gmail.com>
2023-04-16 16:01:13 +0200

    Added visualization tests

--- MainForm.cs
+++ MainForm.cs
@@ -1,20 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿using GraphAlgorithmVisualizer.Tests;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GraphAlgorithmVisualizer
 {
     public partial class MainForm : Form
     {
+        private readonly Graphics graphics;
+
         public MainForm()
         {
             InitializeComponent();
+
+            PB_Canvas.Image = new Bitmap(PB_Canvas.Width, PB_Canvas.Height);
+            graphics = Graphics.FromImage(PB_Canvas.Image);
+            graphics.FillRectangle(new SolidBrush(PB_Canvas.BackColor), 0, 0, PB_Canvas.Width, PB_Canvas.Height);
+
+            //VisualizationTests.GraphDrawingTest(graphics, PB_Canvas.Width, PB_Canvas.Height);
+            VisualizationTests.GraphPositioningTest(graphics, PB_Canvas.Width, PB_Canvas.Height);
         }
     }
 }
--- Tests/VisualizationTests.cs
+++ Tests/VisualizationTests.cs
@@ -1,10 +1,60 @@
-﻿namespace GraphAlgorithmVisualizer.Tests
+﻿using GraphAlgorithmVisualizer.MathObjects;
+using System.Drawing;
+
+namespace GraphAlgorithmVisualizer.Tests
 {
     internal class VisualizationTests
     {
-        public static void GraphDrawingTest()
+        public static void GraphDrawingTest(Graphics graphics, int pictureBoxWidth, int pictureBoxHeight)
         {
+            Graph graph = new Graph(true, 9);
+            /*graph.AddEdge(graph[0], graph[1], 4);
+            graph.AddEdge(graph[0], graph[2], 2);
 
+            graph.AddEdge(graph.V(1), graph.V(2), 3);
+            graph.AddEdge(graph.GetVertex(1), graph[3], 2);
+            graph.AddEdge(graph[1], graph[4], 3);
+
+            graph.AddEdge(graph.V(2), graph.V(1), 1);
+            graph.AddEdge(graph.V(2), graph.V(3), 4);
+            graph.AddEdge(graph.V(2), graph.V(4), 5);
+
+            graph.AddEdge(graph[4], graph.GetVertex(3), 1);*/
+
+            for (int i = 1; i < graph.VerticesCount; i++)
+                graph.AddEdge(graph[0], graph[i], i * 2);
+
+            int offset = 50;
+            graph[0].MoveTo(pictureBoxWidth / 2, pictureBoxHeight / 2);
+            graph[1].MoveTo(graph[0].X - offset, graph[0].Y - offset);
+            graph[2].MoveTo(graph[0].X + offset, graph[0].Y - offset);
+            graph[3].MoveTo(graph[0].X - offset, graph[0].Y + offset);
+            graph[4].MoveTo(graph[0].X + offset, graph[0].Y + offset);
+
+            graph[5].MoveTo(graph[0].X, graph[0].Y - offset * 2);
+            graph[6].MoveTo(graph[0].X + offset * 2, graph[0].Y);
+            graph[7].MoveTo(graph[0].X, graph[0].Y + offset * 2);
+            graph[8].MoveTo(graph[0].X - offset * 2, graph[0].Y);
+
+            //graph[1, 2].Draw(graphics);
+
+            //graph[0].Draw(graphics);
+            //graph[1].Draw(graphics);
+            //graph[2].Draw(graphics);
+            //graph[3].Draw(graphics);
+            //graph[4].Draw(graphics);
+
+            graph.Draw(graphics);
+        }
+        public static void GraphPositioningTest(Graphics graphics, int pictureBoxWidth, int pictureBoxHeight)
+        {
+            Graph graph = new Graph(true, 8);
+            for (int i = 1; i < 8; i++)
+            {
+                graph.AddEdge(graph[0], graph[i], i * 3);
+            }
+            graph.MoveTo(pictureBoxWidth / 2, pictureBoxHeight / 2);
+            graph.Draw(graphics);
         }
     }
 }
