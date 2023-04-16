1e23dc33ec2dd6bec25bb64d9f7ad491377ef8c0
MaxKolac <maksymilian.kolacinski@gmail.com>
2023-04-16 16:02:00 +0200

    Added Extensions.cs

---
+++ Extensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace GraphAlgorithmVisualizer
+{
+    internal class Extensions
+    {
+        public static double ToRadians(double degrees) => degrees * (Math.PI / 180d);
+        public static double ToDegrees(double radians) => radians * (180d / Math.PI);
+    }
+}
