f74bb9dc5950a09b779185b5004d346c8c373b13
MaxKolac <maksymilian.kolacinski@gmail.com>
2023-04-22 14:11:27 +0200

    Added ISelectable interface

---
+++ Visualization/ISelectable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GraphAlgorithmVisualizer.Visualization
+{
+    /// <summary>
+    /// Interface implemented by objects that appear on the main Canvas and can be interacted with, moved around and such.
+    /// </summary>
+    internal interface ISelectable
+    {
+        void DrawSelectedOutline();
+        void FollowCursow();
+    }
+}
