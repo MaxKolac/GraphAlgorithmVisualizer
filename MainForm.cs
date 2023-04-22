99ce43f06b899b2a81ce8c5ffc1db312f72282c3
MaxKolac <maksymilian.kolacinski@gmail.com>
2023-04-22 14:55:49 +0200

    Added another undistanced example Graph

--- MainForm.cs
+++ MainForm.cs
@@ -14,7 +14,6 @@ namespace GraphAlgorithmVisualizer
         public MainForm()
         {
             InitializeComponent();
-
             PB_Canvas.Image = new Bitmap(PB_Canvas.Width, PB_Canvas.Height);
             graphics = Graphics.FromImage(PB_Canvas.Image);
             graphics.FillRectangle(new SolidBrush(PB_Canvas.BackColor), 0, 0, PB_Canvas.Width, PB_Canvas.Height);
@@ -23,7 +22,7 @@ namespace GraphAlgorithmVisualizer
             //VisualizationTests.GraphPositioningTest(graphics, PB_Canvas.Width, PB_Canvas.Height);
             //VisualizationTests.ArrowTest(graphics, PB_Canvas.Width, PB_Canvas.Height);
             //VisualizationTests.CurvableLineTest(graphics, PB_Canvas.Width, PB_Canvas.Height);
-            graph = VisualizationTests.ExampleGraph();
+            graph = VisualizationTests.ExampleDistancedGraph(true);
             graph.SetPosition(PB_Canvas.Width / 2, PB_Canvas.Height / 2);
             graph.Draw(graphics);
         }
--- Tests/VisualizationTests.cs
+++ Tests/VisualizationTests.cs
@@ -111,20 +111,37 @@ namespace GraphAlgorithmVisualizer.Tests
             }
         }
 
-        public static Graph ExampleGraph()
+        public static Graph ExampleDistancedGraph(bool isDirectional)
         {
-            Graph graph = new Graph(true, true, 7);
+            Graph graph = new Graph(isDirectional, true, 7);
             graph.AddEdge(graph.V(0), graph.V(1), 4);
-            graph.AddEdge(graph.V(1), graph.V(2), 2);
+            graph.AddEdge(graph.V(1), graph.V(2), 15);
             graph.AddEdge(graph.V(2), graph.V(3), 1);
             graph.AddEdge(graph.V(3), graph.V(4), 4);
             graph.AddEdge(graph.V(4), graph.V(5), 5);
-            graph.AddEdge(graph.V(5), graph.V(6), 3);
+            graph.AddEdge(graph.V(5), graph.V(6), 12);
             graph.AddEdge(graph.V(6), graph.V(0), 2);
 
-            graph.AddEdge(graph.V(2), graph.V(4), 1);
-            graph.AddEdge(graph.V(1), graph.V(5), 1);
+            graph.AddEdge(graph.V(2), graph.V(4), 7);
+            graph.AddEdge(graph.V(1), graph.V(5), 21);
             graph.AddEdge(graph.V(3), graph.V(6), 1);
+            return graph;
+        }
+
+        public static Graph ExampleUndistancedGraph(bool isDirectional)
+        {
+            Graph graph = new Graph(isDirectional, false, 7);
+            graph.AddEdge(graph.V(0), graph.V(1));
+            graph.AddEdge(graph.V(1), graph.V(2));
+            graph.AddEdge(graph.V(2), graph.V(3));
+            graph.AddEdge(graph.V(3), graph.V(4));
+            graph.AddEdge(graph.V(4), graph.V(5));
+            graph.AddEdge(graph.V(5), graph.V(6));
+            graph.AddEdge(graph.V(6), graph.V(0));
+
+            graph.AddEdge(graph.V(2), graph.V(4));
+            graph.AddEdge(graph.V(1), graph.V(5));
+            graph.AddEdge(graph.V(3), graph.V(6));
             return graph;
         }
     }
